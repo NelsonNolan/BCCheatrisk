26e1b90ea8760805a6b21130db5f3dbc596ea2bf
NelsonNolan <nelson.nolan.nn@gmail.com>
2024-09-25 23:07:36 +1000

    Update JAVA-SA1.js

--- JAVA-SA1.js
+++ JAVA-SA1.js
@@ -1,4 +1,4 @@
-fetch('"C:\Users\nelso\OneDrive\Documents\Heat risk dashboard html\SA1_BLACKTOWN.geojson"')
+fetch('"SA1_BLACKTOWN.geojson"')
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data).addTo(map); 
